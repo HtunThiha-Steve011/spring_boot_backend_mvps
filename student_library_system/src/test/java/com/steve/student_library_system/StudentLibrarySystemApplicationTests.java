f2aa6c91fd7d21bad4043c9171c538c41132a545
Htun Thiha (Steve) <htunthiha.dev01@gmail.com>
2025-08-09 13:19:20 +0800

    (new): Initial setup of student_library_system
    Test API: GET /api/v1
    req: none
    res: "Hello World"

---
+++ student_library_system/src/test/java/com/steve/student_library_system/StudentLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.student_library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
