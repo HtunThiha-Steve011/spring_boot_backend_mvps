13c243af46218c735a9ea81fc16740468917b0ff
Htun Thiha (Steve) <htunthiha.dev01@gmail.com>
2025-08-12 14:33:49 +0800

    setup(bug_bounty_system): Initial setup of Spring Boot Application

---
+++ bug_bounty_system/src/main/java/com/steve/bug_bounty_system/BugBountySystemApplication.java
@@ -0,0 +1,13 @@
+package com.steve.bug_bounty_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugBountySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugBountySystemApplication.class, args);
+	}
+
+}
---
+++ bug_bounty_system/src/test/java/com/steve/bug_bounty_system/BugBountySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.bug_bounty_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugBountySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
