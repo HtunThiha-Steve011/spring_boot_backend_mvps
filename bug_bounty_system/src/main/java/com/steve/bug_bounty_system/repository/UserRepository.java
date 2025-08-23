a39708db7c69d5adc4981ccfa57a333483175342
Htun Thiha (Steve) <htunthiha.dev01@gmail.com>
2025-08-23 22:09:07 +0800

    Delete bug_bounty_system

--- bug_bounty_system/src/main/java/com/steve/bug_bounty_system/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.steve.bug_bounty_system.repository;
-
-import com.steve.bug_bounty_system.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
