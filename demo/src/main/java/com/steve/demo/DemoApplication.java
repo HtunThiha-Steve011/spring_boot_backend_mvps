c9876b32e1f0d50c60cf1de98450e9bf607033e0
Htun Thiha (Steve) <htunthiha.dev01@gmail.com>
2025-08-30 11:06:24 +0800

    new(demo): Add two CLI runners in custom @Order

--- demo/src/main/java/com/steve/demo/DemoApplication.java
+++ demo/src/main/java/com/steve/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.steve.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,22 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	// second CLI runner
+	@Bean
+	@Order(value = 2)
+	public CommandLineRunner runCliProgram() {
+		String[] cliArgs = new String[]{"Apple", "Orange", "Cherry"};
+		return newArgs -> {
+			for (int i = 0; i < cliArgs.length; i++) {
+				System.out.println(String.format("#%d fruit: %s", i, cliArgs[i]));
+			}
+		};
+    }
+
+	// first CLI runner
+	@Bean
+	@Order(value = 1)
+	public CommandLineRunner runHelloWorld() {
+		return args -> System.out.println("Hello World");
+	}
 }
